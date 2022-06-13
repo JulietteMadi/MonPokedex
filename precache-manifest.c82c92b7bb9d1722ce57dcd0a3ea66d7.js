9d5f80c620d0090b7956573f2ff85a677a4b05b3
Juliette Madinier <juliette.madinier@gmail.com>
2022-06-13 17:26:10 +0200

    Mise en production

---
+++ precache-manifest.c82c92b7bb9d1722ce57dcd0a3ea66d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f80e5ffbf2459e2d905e5d07a04e9d05",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ad9f6a953b69037029e7",
+    "url": "/static/js/2.33578c86.chunk.js"
+  },
+  {
+    "revision": "70884f2377ceb6920c9d",
+    "url": "/static/js/3.8be14b25.chunk.js"
+  },
+  {
+    "revision": "847ac41a73e110715841",
+    "url": "/static/js/main.2fda4f2e.chunk.js"
+  },
+  {
+    "revision": "c9a94f93714f1730a1fe",
+    "url": "/static/js/runtime~main.0a3e5500.js"
+  }
+]);
