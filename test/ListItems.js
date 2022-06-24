94b1cbb39c4058e927522e6c86c1485334cf0d46
JulietteMadi <juliette.madinier@gmail.com>
2022-06-24 16:31:56 +0100

    Mise en prod responsive

---
+++ test/ListItems.js
@@ -0,0 +1,19 @@
+export const ListItems = [
+    {
+        Name : "Verre à pied",
+        Rate : 3,
+        Price : 12        
+    },
+
+    {
+        Name : "Carafe en céramique",
+        Rate : 4,
+        Price : 17        
+    },
+
+    {
+        Name : "Salière indienne",
+        Rate : 4,
+        Price : 14      
+    }
+]
